a85afd4a2eb9e87319deac5ed3932246a93ca002
Maksim1-smg <pmaks2365@gmail.com>
2024-02-13 07:19:38 +0700

    feat:select anime + fix:not-found

--- frontend/app/title/index.tsx
+++ frontend/app/title/index.tsx
@@ -6,13 +6,29 @@ import {ScreenView} from "../../src/styles/global";
 import {colors} from "../../src/styles/variables";
 import {TitleItem} from '../../src/components/home-screen/title-item';
 import {RootState} from "../../src/store";
+import { View } from "react-native";
+import React, { useState } from "react";
+import RNPickerSelect from 'react-native-picker-select';
 
 const TitlePage = () => {
   const title = useSelector((state: RootState) => state.title.title);
 
-  if (!title)
+const [selectedValue, setSelectedValue] = useState('');
+
+  const items = [
+    { label: 'Просмотренно', value: 'viewed' },
+    { label: 'В планах', value: 'planned' },
+    { label: 'Заброшенно', value: 'abandoned' },
+    { label: 'Избранное', value: 'favorite' },
+    { label: 'Смотрю', value: 'watching' },
+  ];
+
+  if (title === null)
     return router.push('/not-found')
 
+  if (title?.player?.alternative_player === null || title?.player?.alternative_player === '')
+    return router.push('/not-found')
+  
   const {player} = title
 
   const htmlContent = `
@@ -28,8 +44,13 @@ const TitlePage = () => {
       * {
         margin: 0;
         padding: 0;
-        height: 100vh;
+        height: 100%;
         background-color: ${colors.background};
+      }
+
+      body {
+        display: flex;
+        flex-direction: column;
       }
     </style>
     <body>
@@ -48,6 +69,13 @@ const TitlePage = () => {
   return (
     <ScreenView>
       <TitleItem title={title}/>
+      <View>
+        <RNPickerSelect
+          onValueChange={(value) => setSelectedValue(value)}
+          items={items}
+          placeholder={{ label: 'Сохранить в заметки...', value: 'null' }}
+        />
+      </View>
       <WebView
         style={{ marginBottom: 100, height: 300, width: '100%'}}
         source={{html: htmlContent}}
